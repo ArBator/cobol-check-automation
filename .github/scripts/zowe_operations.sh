45287351d01dbf466562c2f2e2f7e7766f553451
ArBator <36726168+ArBator@users.noreply.github.com>
2026-07-14 14:06:51 +0200

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -4,8 +4,8 @@
 # Convert username to lowercase
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
-# Közös csatlakozási paraméterek (így nem kell minden parancshoz kiírni)
-ZOWE_PARAMS="--host YOUR_HOST --port YOUR_PORT --user $ZOWE_USERNAME --pass $ZOWE_PASSWORD --reject-unauthorized false"
+# ITT VAN A LÉNYEG 2: Itt olvassa ki a bash szkript az átadott adatokat
+ZOWE_PARAMS="--host $YOUR_HOST --port $YOUR_PORT --user $ZOWE_USERNAME --pass $ZOWE_PASSWORD --reject-unauthorized false"
 
 # Check if directory exists, create if it doesn't
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" $ZOWE_PARAMS &>/dev/null; then
@@ -15,7 +15,7 @@ else
   echo "Directory already exists."
 fi
 
-# Upload files (itt javítva lettek a hiányzó szóközök!)
+# Upload files
 zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.9.jar" $ZOWE_PARAMS
 
 # Verify upload
