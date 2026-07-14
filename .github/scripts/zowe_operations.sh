9f7ec874f1c9634c4bc99e35420da9c0cf400307
ArBator <36726168+ArBator@users.noreply.github.com>
2026-07-14 15:53:14 +0200

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -2,23 +2,19 @@
 # zowe_operations.sh
 
 # Convert username to lowercase
-LOWERCASE_USERNAME= "Z71062"  #$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
-
-# ITT VAN A LÉNYEG 2: Itt olvassa ki a bash szkript az átadott adatokat
-#ZOWE_PARAMS="--host $ZOWE_HOST --port $ZOWE_PORT --user $ZOWE_USERNAME --pass $ZOWE_PASSWORD --reject-unauthorized false"
-ZOWE_PARAMS="--host $ZOWE_HOST --port $ZOWE_PORT --user "Z71062" --pass $ZOWE_PASSWORD --reject-unauthorized false"
+LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
 # Check if directory exists, create if it doesn't
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" $ZOWE_PARAMS &>/dev/null; then
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
   echo "Directory does not exist. Creating it..."
-  zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck" $ZOWE_PARAMS
+  zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck"
 else
   echo "Directory already exists."
 fi
 
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.9.jar" $ZOWE_PARAMS
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.9.jar"
 
 # Verify upload
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" $ZOWE_PARAMS
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
